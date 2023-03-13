bab4d521d90ef952cd23a027fa0a3d975d3a7bea
megan <mmeiser19@gmail.com>
2023-03-13 13:58:04 -0400

    yay we got pipes to work (we forgot to reset a variable to 0)

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ void redirectOutput(char **args, char *command);
 char *commands[7] = {"ls", "cd", "pwd", "exit", "grep", "cat","help"};
 
 int main(void) {
-    char *args[MAX_LINE/2 + 1]; /* command line arguments */
+    char *args[MAX_LINE / 2 + 1]; /* command line arguments */
     int argc = 0; //counter for number of arguments
     int line = 0; //line number printed for every command prompt
     int numCommands = getLength(commands);
@@ -46,86 +46,81 @@ int main(void) {
         while (token != NULL) {
             args[i++] = token;
             token = strtok(NULL, " ");
-	    argc++;
+            argc++;
         }
         args[i] = NULL; // set the last element to null pointer
 
-	//split args check modifier
-	int haspipe = 0;
-	for(int i = 0; i < argc; i++){
-	    if(strcmp(args[i], "|") == 0){
-		haspipe = 1;
-	    }
-	}
+        //split args check modifier
+        int haspipe = 0;
+        for (int i = 0; i < argc; i++) {
+            if (strcmp(args[i], "|") == 0) {
+                haspipe = 1;
+            }
+        }
 
-   	//check for multiple args, replacement values
-    	char *repargs[MAX_LINE/2 + 1];
-	int repargc = 0;
-	int fint;
-	if(haspipe == 1){
+        //check for multiple args, replacement values
+        char *repargs[MAX_LINE / 2 + 1];
+        int repargc = 0;
+        int fint;
+        if (haspipe == 1) {
 
-	    //printf("%d", argc);
+            //printf("%d", argc);
             //read and fork if pipe symbol
-	    for(int i = 0; i < argc; i++){
-	        //check if arg is pipe
-	        if(strcmp(args[i], "|") == 0){
-		    //create new process
-		    fint = fork();
-		    //erase replacement values 
-		    for(int j = 0; j < repargc; j++){
-		        repargs[j] = NULL;
-		    }
-		    repargc = 0;
-	        }
-	        else{
-		    repargs[repargc] = args[i];
-		    repargc++;
-	        }
-	    }
-
-	    //initialize forked pipe process
-	    if(fint == 0){
-	        for(int i = 0; i < (MAX_LINE/2 + 1); i++){
-   	            args[i] = NULL;
+            for (int i = 0; i < argc; i++) {
+                //check if arg is pipe
+                if (strcmp(args[i], "|") == 0) {
+                    //create new process
+                    fint = fork();
+                    //erase replacement values
+                    for (int j = 0; j < repargc; j++) {
+                        repargs[j] = NULL;
+                    }
+                    repargc = 0;
+                } else {
+                    repargs[repargc] = args[i];
+                    repargc++;
                 }
-                for(int i = 0; i < repargc; i++){
+            }
+
+            //initialize forked pipe process
+            if (fint == 0) {
+                for (int i = 0; i < (MAX_LINE / 2); i++) {
+                    args[i] = NULL;
+                }
+                for (int i = 0; i < repargc; i++) {
                     args[i] = repargs[i];
-	        }
-	        argc = repargc;
-	    }
-	    else{	
-	       int erase = 0; //false by default
-	        for(int i = 0; i < argc; i++){
-		    if(erase == 1){
-		        args[i] = NULL;
-		    }
-		    else if(strcmp(args[i], "|") == 0){
-		        args[i] = NULL;
-		        argc = i - 1;
-		        erase = 1;
-		        break;
-		    }
-		    else{
-		        //this spot intentionally left blank
-		    }
-	        }
-	    }
-	}
+                }
+                argc = repargc;
+            }
+                //erase arguments after pipe in parent process from args array
+            else {
+                int erase = 0; //false by default
+                for (int i = 0; i < argc; i++) {
+                    if (erase == 1) {
+                        args[i] = NULL;
+                    } else if (strcmp(args[i], "|") == 0) {
+                        args[i] = NULL;
+                        argc = i;// - 1;
+                        erase = 1;
+                        //break;
+                    } else {
+                        //this spot intentionally left blank
+                    }
+                }
+            }
+        }
 
         //if the user enters nothing, continue
         if (strlen(input) == 0) {
             continue;
-        }
-        else if (strcmp(args[0], "exit") == 0) {
+        } else if (strcmp(args[0], "exit") == 0) {
             exit(0);
-        }
-        else if (strcmp(args[0], "help") == 0) {
+        } else if (strcmp(args[0], "help") == 0) {
             printf("Commands:\n");
             for (int j = 0; j < numCommands; j++) {
                 printf("%s\n", commands[j]);
             }
-        }
-        else if (strcmp(input, "pwd") == 0) {
+        } else if (strcmp(input, "pwd") == 0) {
             char cwd[100];
             if (getcwd(cwd, sizeof(cwd)) != NULL) {
                 if (checkRedirectOutput(args) == 1) {
@@ -135,24 +130,21 @@ int main(void) {
                     fprintf(output, "%s", cwd);
                     fclose(output);*/
                     redirectOutput(args, cwd);
-                }
-                else {
+                } else {
                     printf("%s\n", cwd);
                 }
             }
-        }
-        else if (strncmp(input, "cd", 2) == 0) {
+        } else if (strncmp(input, "cd", 2) == 0) {
             char *path = input + 3; // skip the "cd " prefix
             if (chdir(path) == 0) {
                 printf("Changed directory to: %s\n", path);
             } else {
                 perror("chdir() error");
             }
-        }
-        else if (strcmp(input, "grep") == 0) {
+        } else if (strcmp(input, "grep") == 0) {
             //use the grep command
         }
-        //base case for commands such as ls, cat
+            //base case for commands such as ls, cat
         else {
             if (fork() == 0) {
                 if (checkRedirectOutput(args) == 1) {
@@ -162,7 +154,7 @@ int main(void) {
                         if (strcmp(args[i], ">") == 0) {
                             //open file
                             FILE *output;
-                            output = fopen(args[i+1], "w");
+                            output = fopen(args[i + 1], "w");
                             //print to file
                             fprintf(output, "%s", (char) execvp(args[0], args));
                             fclose(output);
@@ -170,7 +162,7 @@ int main(void) {
                         i++;
                     }
                 }
-                //if last arg is &, run in background
+                    //if last arg is &, run in background
                 else {
                     execvp(args[0], args);
                 }
@@ -178,13 +170,11 @@ int main(void) {
                 if (valid_command(args[0]) == 0) {
                     printf("Unknown command: %s\n", args[0]);
                 }
-            }
-            else {
+            } else {
                 //run process in background
-                if (strcmp(args[argc-1], "&") == 0) {
+                if (strcmp(args[argc - 1], "&") == 0) {
                     continue;
-                }
-                else {
+                } else {
                     wait(NULL);
                 }
                 // Add a newline at the end of the output
@@ -192,20 +182,21 @@ int main(void) {
             }
         }
 
-	if(haspipe == 1){
-	    wait(NULL);
-	}
+        if (haspipe == 1) {
+            wait(NULL);
+        }
         //***** NOT SURE IF NECESSARY, KEEP FOR NOW *****
         //clear input buffer for next command
         memset(input, 0, sizeof(input));
 
-	
-	if(fint == 0 && haspipe == 1){
-	    exit(0);
-	}
 
-	haspipe = 0;
-	
+        if (fint == 0 && haspipe == 1) {
+            exit(0);
+        }
+
+        haspipe = 0;
+        argc = 0; //reset argc
+
     }
 
 }
